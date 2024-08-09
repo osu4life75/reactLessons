7cb681ae6a3501705425a09102fc8040f2f723a4
George Lynch <osu4life75@gmail.com>
2024-08-08 19:49:08 -0500

    working of Footer

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Body from './components/Body'
 import NavBar from './components/NavBar'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import 'mdb-react-ui-kit/dist/js/mdb.min.js';
+
 
 
 export default function App() {
